c50a3dc3d4de65f8e4ac35fd80e1b73903f1e2cd
= <=>
2018-11-22 10:51:54 +0530

    automated plugin installation
    changed from /test/ to home directory

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 wget https://raw.githubusercontent.com/govindKAG/work-vimrc/master/.vimrc
-mv ./.vimrc /tmp/test/.vimrc
+#mv ./.vimrc /tmp/test/.vimrc
+mv ./.vimrc ~/.vimrc
+vim -c ":PlugInstall"
